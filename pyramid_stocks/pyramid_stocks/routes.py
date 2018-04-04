0620cc0d6acc244d03418b0fcfd14f86b6c93fb6
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-04 15:06:48 -0700

    added html files and converted to jinja

--- pyramid_stocks/pyramid_stocks/routes.py
+++ pyramid_stocks/pyramid_stocks/routes.py
@@ -1,3 +1,8 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.add_route('index', '/')
+    config.add_route('register', '/auth')
+    config.add_route('login', '/auth')
+    config.add_route('portfilio', '/stock')
+    config.add_route('stock-add', '/portfolio/{symbol}')
+    config.add_route('stock-detail', '/portfolio{symbol}')
