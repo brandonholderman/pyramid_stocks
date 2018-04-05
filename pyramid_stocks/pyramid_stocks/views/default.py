f83ddd844e75719d1136c61baf0c47384657af03
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-05 10:43:44 -0700

    cleaned up unused code

--- pyramid_stocks/pyramid_stocks/views/default.py
+++ pyramid_stocks/pyramid_stocks/views/default.py
@@ -1,31 +1,29 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 
-from sqlalchemy.exc import DBAPIError
-
 from ..models import MyModel
 
 
-@view_config(route_name='index', renderer='../templates/index.jinja2')
+@view_config(route_name='index', renderer='../templates/index.jinja2', request_method='GET')
 def index_view(request):
     return {}
 
 
-@view_config(route_name='auth', renderer='../templates/auth.jinja2')
+@view_config(route_name='auth', renderer='../templates/auth.jinja2', request_method='GET')
 def auth_view(request):
     return {}
 
 
-@view_config(route_name='portfolio', renderer='../templates/portfolio.jinja2')
+@view_config(route_name='portfolio', renderer='../templates/portfolio.jinja2', request_method='GET')
 def portfolio_view(request):
     return {}
 
 
-@view_config(route_name='add', renderer='../templates/add.jinja2')
+@view_config(route_name='add', renderer='../templates/add.jinja2', request_method='GET')
 def add_view(request):
     return {}
 
 
-@view_config(route_name='detail', renderer='../templates/detail.jinja2')
+@view_config(route_name='detail', renderer='../templates/detail.jinja2', request_method='GET')
 def detail_view(request):
     return {}
