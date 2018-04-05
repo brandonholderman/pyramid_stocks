80cc360b870038af0750b2779cb78a81ca38aebd
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-05 14:52:52 -0700

    commit before branch change

--- pyramid_stocks/pyramid_stocks/views/default.py
+++ pyramid_stocks/pyramid_stocks/views/default.py
@@ -1,18 +1,34 @@
 from pyramid.response import Response
 from pyramid.view import view_config
+from pyramid.response import Response
+from pyramid.httpexceptions import HTTPFound, HTTPNotFound
 
 from ..models import MyModel
 
 
 @view_config(route_name='index', renderer='../templates/index.jinja2', request_method='GET')
 def index_view(request):
-    return {}
+    return Response('I did a thing')
 
 
-@view_config(route_name='auth', renderer='../templates/auth.jinja2', request_method='GET')
+@view_config(route_name='auth', renderer='../templates/auth.jinja2')
 def auth_view(request):
-    return {}
-
+    try:
+        if request.method == 'GET':
+            username = request.GET('username')
+            password = request.GET('password')
+            print('User: {}, Pass: {}' .format(username, password))
+            return HTTPFound(location=request.route_url('index'))
+    except KeyError:
+        return {}
+    
+    if request.method == 'POST':
+            username = request.POST('username')
+            email = request.POST
+            password = request.POST('password')
+            print('User: {}, Pass: {}, Email: {}' .format(username, password, email))
+            return HTTPFound(location=request.route_url('index'))
+    return HTTPNotFound()
 
 @view_config(route_name='portfolio', renderer='../templates/portfolio.jinja2', request_method='GET')
 def portfolio_view(request):
