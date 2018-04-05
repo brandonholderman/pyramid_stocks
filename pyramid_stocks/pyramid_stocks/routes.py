b9606600b29a393db0f397e28710c90af035fe34
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-05 09:35:47 -0700

    changed nav

--- pyramid_stocks/pyramid_stocks/routes.py
+++ pyramid_stocks/pyramid_stocks/routes.py
@@ -1,8 +1,7 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('index', '/')
-    config.add_route('register', '/auth')
-    config.add_route('login', '/auth')
-    config.add_route('portfilio', '/stock')
+    config.add_route('auth', '/auth')
+    config.add_route('portfilio', '/portfolio')
     config.add_route('stock-add', '/portfolio/{symbol}')
     config.add_route('stock-detail', '/portfolio{symbol}')
