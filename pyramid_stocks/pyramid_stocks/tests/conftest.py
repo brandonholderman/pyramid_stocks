533ea441649e65d574f9b6bb289c700b1b473ba0
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-10 11:06:36 -0700

    working, need to implement tests

--- pyramid_stocks/pyramid_stocks/tests/conftest.py
+++ pyramid_stocks/pyramid_stocks/tests/conftest.py
@@ -4,3 +4,40 @@ from pyramid import testing
 @pytest.fixture
 def dummy_request():
     return testing.DummyRequest()
+
+@pytest.fixture
+def create_new_entry():
+    pass
+
+@pytest.fixture
+def configuration(request):
+    config = testing.setUp(settings={
+        'sqlalchemy.url': 'postgres://localhost:5432/entries_test'
+    })
+
+    config.include('pyramid_stocks.models')
+    config.include('pyramid_stocks.routes')
+
+    def teardown():
+        testing.tearDown()
+
+    request.addfinalizers(teardown)
+    return config
+
+@pytest.fixture
+def db_session(configuration, request):
+    SessionFactory = configuration.registry['dbsession_factory']
+    session = SessionFactory()
+    engine = session.bind
+    Base.metadata.create_all(engine)
+
+    def teardown():
+        session.transaction.rollback()
+        base.metadata.drop_all(engine)
+
+    request.addfinalizers(teardown)
+    return session
+
+@pytest.fixture
+def dummy_request(db_session):
+    pass
