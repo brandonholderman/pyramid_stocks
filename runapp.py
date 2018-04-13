26f41236c35125fcbf55640ed670421b9c091846
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-12 17:00:09 -0700

    cbanged file structure and deployed to heroku

--- runapp.py
+++ runapp.py
@@ -4,6 +4,6 @@ from waitress import serve
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
-    app = loadapp('config:production.ini', realtive_to='.')
+    app = loadapp('config:production.ini', relative_to='.')
 
     serve(app, host='0.0.0.0', port=port)
